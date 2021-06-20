31b82dcad8ba888a56f36a8184ac1fc3199620f3
Regulae <engelhardt@noiseag.ch>
2021-06-20 10:51:13 +0200

    Initial Commit

---
+++ MyMedis/MyMedisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMedisApp.swift
+//  MyMedis
+//
+//  Created by Regula Susan Heisch on 20.06.21.
+//
+
+import SwiftUI
+
+@main
+struct MyMedisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
