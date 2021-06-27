6d955e80f346dedc64db5a99a1090d1c8fbd38be
Regula Heisch <regula@heisch.ch>
2021-06-27 16:52:42 +0200

    init widget

---
+++ MyMedisWidget/WidgetView.swift
@@ -0,0 +1,20 @@
+//
+//  WidgetView.swift
+//  MyMedis
+//
+//  Created by Regula Susan Heisch on 27.06.21.
+//
+
+import SwiftUI
+
+struct WidgetView: View {
+    var body: some View {
+        MedicationOverview()
+    }
+}
+
+struct WidgetView_Previews: PreviewProvider {
+    static var previews: some View {
+        WidgetView()
+    }
+}
