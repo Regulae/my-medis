f4087f3cf0bb1e82b2454d9e5160c84172f0da88
Regula Heisch <regula@heisch.ch>
2021-08-20 16:18:11 +0200

    order widgetview

--- MedicationWidget/WidgetView.swift
+++ MedicationWidget/WidgetView.swift
@@ -11,19 +11,6 @@ import CoreData
 
 struct WidgetView: View {
 
-    var body: some View {
-        VStack {
-            Text("Today")
-                    .font(.headline)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            ForEach(meds) { medication in
-                Text("\(medication.name!)")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-            }
-        }
-        .padding()
-    }
-
     var meds: [Medication] {
         let request = NSFetchRequest<Medication>(entityName: "Medication")
         do {
@@ -32,6 +19,70 @@ struct WidgetView: View {
             print(error.localizedDescription)
             return []
         }
+    }
+
+    var morningMedications: [Medication] {
+        meds.filter { medication in
+            medication.daytime == "Morning"
+        }
+    }
+    var lunchMedications: [Medication] {
+        meds.filter { medication in
+            medication.daytime == "Lunch"
+        }
+    }
+    var eveningMedications: [Medication] {
+        meds.filter { medication in
+            medication.daytime == "Evening"
+        }
+    }
+    var nightMedications: [Medication] {
+        meds.filter { medication in
+            medication.daytime == "Night"
+        }
+    }
+
+    var body: some View {
+        VStack {
+            Text("Today")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            if morningMedications.count >= 1 {
+                Text("Morgen")
+                        .bold()
+                ForEach(morningMedications) { medication in
+                    Text("\(medication.name!)")
+                }
+                Divider()
+            }
+            if lunchMedications.count >= 1 {
+                Text("Mittag")
+                        .bold()
+                ForEach(lunchMedications) { medication in
+                    Text("\(medication.name!)")
+                }
+                Divider()
+            }
+            if eveningMedications.count >= 1 {
+                Text("Abend")
+                        .bold()
+                ForEach(eveningMedications) { medication in
+                    Text("\(medication.name!)")
+                }
+                Divider()
+
+            }
+            if nightMedications.count >= 1 {
+                Text("Nacht")
+                        .bold()
+                ForEach(nightMedications) { medication in
+                    Text("\(medication.name!)")
+                }
+                Divider()
+            }
+
+        }
+        .padding()
     }
 }
 
